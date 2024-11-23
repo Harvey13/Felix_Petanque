40af21a30eb5a4a3c2997d9c148bd6f76bb4aa10
Harvey13 <h.pipon@wanadoo.fr>
2024-11-23 16:57:19 +0100

    Amélioration du service worker pour une meilleure gestion hors ligne

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,16 +4,31 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/petanque.svg',
-  '/assets/index.css',
-  '/assets/index.js'
+  '/assets/index-Dh29RR78.js',
+  '/assets/index-Vg7VwxWX.css'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
@@ -24,17 +39,24 @@ self.addEventListener('fetch', (event) => {
         if (response) {
           return response;
         }
-        return fetch(event.request)
+
+        return fetch(event.request.clone())
           .then((response) => {
             if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
+
             const responseToCache = response.clone();
             caches.open(CACHE_NAME)
               .then((cache) => {
                 cache.put(event.request, responseToCache);
               });
+
             return response;
+          })
+          .catch(() => {
+            // Retourner une réponse par défaut si hors ligne
+            return new Response('Vous êtes hors ligne. L\'application continuera de fonctionner avec les données en cache.');
           });
       })
   );
